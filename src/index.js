7c868cf592ca13bb84c8dbd379e12cb11d6f3df9
sergei-1973 <baragozik00@gmail.com>
2025-01-03 13:08:27 +0300

    add modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import { pow2, pow3, pow4 } from "./modules/math.mjs";
+
 function component(text) {
   const element = document.createElement("h1");
   element.textContent = text;
   return element;
 }
 document.body.prepend(component("Set Webpack"));
+
+console.log(pow2(2) + pow3(2) + pow4(2));
---
+++ src/modules/math.mjs
@@ -0,0 +1,9 @@
+export function pow2(num) {
+  return Math.pow(num, 2);
+}
+export function pow3(num) {
+  return Math.pow(num, 3);
+}
+export function pow4(num) {
+  return Math.pow(num, 4);
+}
